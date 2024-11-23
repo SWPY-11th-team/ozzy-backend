665e5d0a10cc073b0cf96e7e1b082658b3d3ba8f
dbstjrals <soekmin1998@naver.com>
2024-11-23 16:00:42 +0900

    First Add

---
+++ src/main/java/com/example/ozzy/OzzyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ozzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ozzy/OzzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ozzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
