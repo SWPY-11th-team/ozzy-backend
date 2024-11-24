e24e1ed1c3c3721a86298c390a78279cb24bf511
chansung <pork1375@gmail.com>
2024-11-25 00:56:10 +0900

    Test : server call

--- src/main/java/com/example/ozzy/test/controller/TestController.java
+++ src/main/java/com/example/ozzy/test/controller/TestController.java
@@ -3,19 +3,37 @@ package com.example.ozzy.test.controller;
 import com.example.ozzy.test.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/test")
 public class TestController {
 
     private final TestService testService;
 
-    @GetMapping("/test")
+    @GetMapping("")
     public void test() {
         testService.first();
+    }
+
+    @GetMapping("/view")
+    public String view() {
+        return "index";
+    }
+
+    @GetMapping("/map")
+    public ResponseEntity<Map<String, String>> map() {
+        return new ResponseEntity<>(Map.of("1", "hi"), HttpStatus.OK);
     }
 
 }
