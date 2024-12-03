95b0cfd9877f294da08ba900f525dc9f2cd989a2
chansung <pork1375@gmail.com>
2024-12-04 00:03:10 +0900

    fix : test

--- src/main/java/com/example/ozzy/OzzyApplication.java
+++ src/main/java/com/example/ozzy/OzzyApplication.java
@@ -9,6 +9,7 @@ public class OzzyApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World! 22");
 		SpringApplication.run(OzzyApplication.class, args);
 	}
 
