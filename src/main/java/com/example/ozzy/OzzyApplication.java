ecfcb46f097612db62a32e1d4dd5606b1d17b8c2
chansung <pork1375@gmail.com>
2024-12-03 23:47:03 +0900

    fix : yml 수정

--- src/main/java/com/example/ozzy/OzzyApplication.java
+++ src/main/java/com/example/ozzy/OzzyApplication.java
@@ -9,6 +9,7 @@ public class OzzyApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World! 222");
 		SpringApplication.run(OzzyApplication.class, args);
 	}
 
