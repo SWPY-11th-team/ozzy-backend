7f0303570b29b4f38f2cc081f0e79a8801fc1926
chansungpark <chansungpark@autoeveramerica.com>
2024-12-11 16:02:21 +0900

    test : main

--- src/main/java/com/example/ozzy/OzzyApplication.java
+++ src/main/java/com/example/ozzy/OzzyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OzzyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("OzzyApplication.main start");
 		SpringApplication.run(OzzyApplication.class, args);
 	}
 
