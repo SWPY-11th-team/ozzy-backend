27b4e7bce4650445372fc9cb9dec3ac2fd3615a0
chansung <pork1375@gmail.com>
2024-12-14 16:25:53 +0900

    fix : 마이페이지

--- src/main/java/com/example/ozzy/user/dto/response/UserFromDate.java
+++ src/main/java/com/example/ozzy/user/dto/response/UserFromDate.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 @ToString
 public class UserFromDate {
 
+    private String name;    // 닉네임
     private int todayFromDate;  // 가입일부터 오늘까지
     private int diaryCount; // 일기 입력 개수
 
